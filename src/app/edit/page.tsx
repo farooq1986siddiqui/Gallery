6f2cd3c88d3dcf20b832fa8e2e5097606ac31cc4
Farooq Ahmad Siddiqui <125255480+farooq1986siddiqui@users.noreply.github.com>
2023-09-28 12:50:27 +0500

    edit feature excluded

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { TabsDemo } from './tabs'
+// import React from 'react'
+// import { TabsDemo } from './tabs'
 
-const Page = ({searchParams: { publicId}}:{searchParams: { publicId:string} }) => {
-    console.log(publicId)
-    return (
-        <>
-        <div className='py-4 px-5'>
-            <h2 className="text-3xl font-semibold tracking-tight">Edit Picture</h2>
-        </div>
-        <TabsDemo src={publicId}/>
-        </>
-    )
-}
+// const Page = ({searchParams: { publicId}}:{searchParams: { publicId:string} }) => {
+//     console.log(publicId)
+//     return (
+//         <>
+//         <div className='py-4 px-5'>
+//             <h2 className="text-3xl font-semibold tracking-tight">Edit Picture</h2>
+//         </div>
+//         <TabsDemo src={publicId}/>
+//         </>
+//     )
+// }
 
-export default Page
+// export default Page
--- src/app/edit/tabs.tsx
+++ src/app/edit/tabs.tsx
@@ -1,142 +1,142 @@
-'use client'
-import { CldImage } from 'next-cloudinary';
+// 'use client'
+// import { CldImage } from 'next-cloudinary';
 
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+// import {
+//   Tabs,
+//   TabsContent,
+//   TabsList,
+//   TabsTrigger,
+// } from "@/components/ui/tabs"
 
-export function TabsDemo({src}:{src:string}) {
-  return (
-    <Tabs defaultValue="original" className="w-full py-4 px-5">
-      <TabsList className="grid w-full grid-cols-5">
-        <TabsTrigger value="original">Original</TabsTrigger>
-        <TabsTrigger value="blur">Blur</TabsTrigger>
-        <TabsTrigger value="gray">Gray Scale</TabsTrigger>
-        <TabsTrigger value="oil">Oil Paint</TabsTrigger>
-        <TabsTrigger value="improve">Improve</TabsTrigger>
-      </TabsList>
-      {/* {original} */}
-      <TabsContent value="original">
-        <div className="flex justify-center py-4 items-center gap-6">      
-            <div>
-            <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-       />
-       </div>
-        <div>
-        <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-       />
-       </div>
-       </div>
-      </TabsContent>
-      {/* {blur} */}
-      <TabsContent value="blur">
-        <div className="flex justify-center py-4 items-center gap-6">      
-            <div>
-            <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-       />
-       </div>
-        <div>
-        <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-         blur={800}
-       />
-       </div>
-       </div>
-      </TabsContent>
-      {/* {grey scale} */}
-      <TabsContent value="gray">
-        <div className="flex justify-center py-4 items-center gap-6">      
-            <div>
-            <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-       />
-       </div>
-        <div>
-        <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-         grayscale={true}
-       />
-       </div>
-       </div>
-      </TabsContent>
-      {/* {oil paint} */}
-      <TabsContent value="oil">
-        <div className="flex justify-center py-4 items-center gap-6">      
-            <div>
-            <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-       />
-       </div>
-        <div>
-        <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-         oilPaint={true,"70"}
-       />
-       </div>
-       </div>
-      </TabsContent>
-      {/* {Improve} */}
-      <TabsContent value="improve">
-        <div className="flex justify-center py-4 items-center gap-6">      
-            <div>
-            <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-       />
-       </div>
-        <div>
-        <CldImage
-         width="500"
-         height="500"
-         src={src}
-         sizes="100vw"
-         alt="Description of my image"
-         improve={true, "50", "indoor"}
-       />
-       </div>
-       </div>
-      </TabsContent>
-    </Tabs>
-  )
-}
+// export function TabsDemo({src}:{src:string}) {
+//   return (
+//     <Tabs defaultValue="original" className="w-full py-4 px-5">
+//       <TabsList className="grid w-full grid-cols-5">
+//         <TabsTrigger value="original">Original</TabsTrigger>
+//         <TabsTrigger value="blur">Blur</TabsTrigger>
+//         <TabsTrigger value="gray">Gray Scale</TabsTrigger>
+//         <TabsTrigger value="oil">Oil Paint</TabsTrigger>
+//         <TabsTrigger value="improve">Improve</TabsTrigger>
+//       </TabsList>
+//       {/* {original} */}
+//       <TabsContent value="original">
+//         <div className="flex justify-center py-4 items-center gap-6">      
+//             <div>
+//             <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//        />
+//        </div>
+//         <div>
+//         <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//        />
+//        </div>
+//        </div>
+//       </TabsContent>
+//       {/* {blur} type ERROR */}
+//       <TabsContent value="blur">
+//         <div className="flex justify-center py-4 items-center gap-6">      
+//             <div>
+//             <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//        />
+//        </div>
+//         <div>
+//         <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//          blur={800}
+//        />
+//        </div>
+//        </div>
+//       </TabsContent>
+//       {/* {grey scale} type ERROR */}
+//       <TabsContent value="gray">
+//         <div className="flex justify-center py-4 items-center gap-6">      
+//             <div>
+//             <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//        />
+//        </div>
+//         <div>
+//         <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//          grayscale={true}
+//        />
+//        </div>
+//        </div>
+//       </TabsContent>
+//       {/* {oil paint} */}
+//       <TabsContent value="oil">
+//         <div className="flex justify-center py-4 items-center gap-6">      
+//             <div>
+//             <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//        />
+//        </div>
+//         <div>
+//         <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//          oilPaint={true,"70"}
+//        />
+//        </div>
+//        </div>
+//       </TabsContent>
+//       {/* {Improve} */}
+//       <TabsContent value="improve">
+//         <div className="flex justify-center py-4 items-center gap-6">      
+//             <div>
+//             <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//        />
+//        </div>
+//         <div>
+//         <CldImage
+//          width="500"
+//          height="500"
+//          src={src}
+//          sizes="100vw"
+//          alt="Description of my image"
+//          improve={true, "50", "indoor"}
+//        />
+//        </div>
+//        </div>
+//       </TabsContent>
+//     </Tabs>
+//   )
+// }
